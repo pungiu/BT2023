c040415701c48cfecdd4e3d4a7efc04795022b33
AAAAAA <galpungartnik41@gmail.com>
2023-04-19 00:49:26 -0700

    drugi commit

---
+++ src/user/fa/user.entity.ts
@@ -0,0 +1,20 @@
+import { Entity } from 'typeorm';
+import { Column, CreateDateColumn, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  first_name: string;
+
+  @Column()
+  last_name: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @CreateDateColumn()
+  create_at: Date;
+}
---
+++ src/user/user.controller.ts
@@ -0,0 +1,33 @@
+import {Controller, Get, Post, Delete, Body, Param, Patch} from '@nestjs/common';
+import {UserService} from "./user.service";
+import {CreateUserDto} from "./dto/create-user.dto";
+import {User} from "./fa/user.entity";
+import {DeleteResult} from "typeorm";
+import {UpdateUserDto} from "./dto/update-user.dto";
+
+@Controller('users')
+export class UserController {
+  constructor(private userService: UserService) {
+  }
+  @Get()
+   async getAll() : Promise<User[]>  {
+    return await this.userService.getAll();
+  }
+  @Get(':id')
+  getById(@Param('id') id: number): Promise<User> {
+    return this.userService.getById(id);
+}
+  @Post()
+  addUser(@Body() createUserDto: CreateUserDto): Promise<User> {
+    return this.userService.addUser(createUserDto);
+  }
+
+  @Delete(':id')
+  deleteUser(@Param('id') id: number): Promise<DeleteResult> {
+    return this.userService.deleteUser(id);
+  }
+  @Patch(':id')
+  updateUser(@Param('id')id: number, @Body() updateUserDto: UpdateUserDto,): Promise<User> {
+    return this.userService.userUpdate(id,updateUserDto);
+  }
+}
